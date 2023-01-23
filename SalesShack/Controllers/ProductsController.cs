5f3a188d17561e9f9e8e10e1116a7bf619135a65
ChrisKLoveless <chriskloveless@gmail.com>
2023-01-23 13:17:15 -0800

    add Edit and Delete functionality to Products
    co-authored-by: Yoonis Ali <yoonismahamoudali@gmail.com>

--- SalesShack/Controllers/ProductsController.cs
+++ SalesShack/Controllers/ProductsController.cs
@@ -41,6 +41,44 @@ namespace SalesShack.Controllers
         _db.SaveChanges();
         return RedirectToAction("Index");
       }
+    }
+
+    public ActionResult Details(int id)
+    {
+      Product thisProduct = _db.Products
+      .Include(product => product.JoinEntities)
+      .ThenInclude(join => join.Sale)
+      .FirstOrDefault(product => product.ProductId == id);
+      return View(thisProduct);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Product thisProduct = _db.Products.FirstOrDefault(product => product.ProductId == id);
+      return View(thisProduct);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Product product)
+    {
+      _db.Products.Update(product);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Product thisProduct = _db.Products.FirstOrDefault(product => product.ProductId == id);
+      return View(thisProduct);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirm(int id)
+    {
+      Product thisProduct = _db.Products.FirstOrDefault(product => product.ProductId == id);
+      _db.Products.Remove(thisProduct);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
-}
 }
