ed44baccdcf3aa4b5634030a90065406481da357
ChrisKLoveless <chriskloveless@gmail.com>
2023-01-24 11:17:02 -0800

    add dates and display data on homepage

---
+++ SalesShack/Migrations/20230124181757_Dates.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesShack.Solution.Migrations
+{
+    public partial class Dates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Promotions",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Promotions",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Promotions",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Promotions");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Promotions");
+
+            migrationBuilder.UpdateData(
+                table: "Promotions",
+                keyColumn: "Title",
+                keyValue: null,
+                column: "Title",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Promotions",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
