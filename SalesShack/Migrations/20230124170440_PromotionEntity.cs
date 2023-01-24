90028abde8886e6094b16bec83ee3da02c1b363b
Yoonis Ali <yoonisali24@Yooniss-MacBook-Air.local>
2023-01-24 09:33:41 -0800

    add full-crud functionality for promotions
    co-authored-by: Chris Loveless <chriskloveless@gmail.com>

---
+++ SalesShack/Migrations/20230124170440_PromotionEntity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesShack.Solution.Migrations
+{
+    public partial class PromotionEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Promotions",
+                columns: table => new
+                {
+                    PromotionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Promotions", x => x.PromotionId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Promotions");
+        }
+    }
+}
---
+++ SalesShack/Migrations/20230124171805_PropertiesForPromotions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesShack.Solution.Migrations
+{
+    public partial class PropertiesForPromotions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Promotions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Promotions_ProductId",
+                table: "Promotions",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Promotions_Products_ProductId",
+                table: "Promotions",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Promotions_Products_ProductId",
+                table: "Promotions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Promotions_ProductId",
+                table: "Promotions");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Promotions");
+        }
+    }
+}
